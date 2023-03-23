f310e54ce012ef0dbf38118b8266babbaaeeba3c
sachinZeroit <121634899+sachinZeroit@users.noreply.github.com>
2023-03-23 09:53:29 +0530

    Add files via upload

---
+++ userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+const user = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    mobile:{
+        type: String,
+        required: true
+    },
+    is_admin:{
+        type:Number,
+        required:true
+    },
+    is_verified:{
+        type:Number,
+        default:0
+    },
+    token:{
+        type:String,
+        default:''
+    },
+},
+{timestamps:true},
+);
+
+module.exports = mongoose.model("User", user) 
